1c0a75ec0661967db624f872f3d2b60488bd8e27
pierman1 <pbleeker@live.nl>
2017-09-05 22:06:44 +0200

    setup loader

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,35 @@
     <navigation>
     </navigation>
     <router-view></router-view>
+    <loader></loader>
   </div>
-
 </template>
 
 <script>
 import Navigation from './components/nav';
 import ShowCase from './components/showcase';
+import Loader from './components/loader';
 
 export default {
   name: 'app',
   components: {
     Navigation,
-    ShowCase
+    ShowCase,
+    Loader
+  },
+  created: function() {
+    setTimeout(function() {
+      const loader = document.querySelector('.loader__container');
+      loader.classList.add('hidden');
+    }, 2000)
   }
 }
 </script>
 
-<style>
+<style scoped>
+
+  .hidden {
+    display: none;
+  }
 
 </style>
--- src/components/loader.vue
+++ src/components/loader.vue
@@ -1,20 +1,19 @@
 <template lang="html">
   <div class="loader__container">
-    <h1>loading...</h1>
+    <h1>...</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'loader',
-  data() {
-
-  }
+  name: 'loader'
 }
 </script>
 
 <style lang="css" scoped>
   .loader__container {
-
+    position: absolute;
+    top: 1.5rem;
+    right: 2rem;
   }
 </style>
