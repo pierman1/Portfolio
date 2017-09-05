92b6bf3f25621abfd2d2ff70f4b9740928588d56
pierman1 <pbleeker@live.nl>
2017-09-05 12:30:27 +0200

    setup main

---
+++ src/components/main.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="main">
+    {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Main'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
